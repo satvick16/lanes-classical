63f4d6a31b5e801eb81b9ec14dcb110b04003727
Satvick16 <satvickacharya16@gmail.com>
2022-11-19 16:00:59 -0500

    forgot to comment out a line -- new framerate = 40fps

--- main.py
+++ main.py
@@ -5,26 +5,16 @@ import time
 
 from PIL import Image, ImageFilter
 
-INPUT = "C:\\Users\\16474\\Desktop\\lanes-utra\\input"
-OUTPUT = "C:\\Users\\16474\\Desktop\\lanes-utra\\output"
+INPUT = "C:\\Users\\16474\\Desktop\\lanes-utra\\lanes-big-in"
+OUTPUT = "C:\\Users\\16474\\Desktop\\lanes-utra\\lanes-big-out"
 
 def contours(img):
-    # get blobs in image
-    # get angle of each blob
     blobs, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # create new image to draw contours on
     img = np.zeros(img.shape, np.uint8)
 
     for blob in blobs:
-        # contour = blob[0]
-
-        # print(blob)
-
-        # if blobs are bigger than a 3x3 square
         if cv2.contourArea(blob) > 100:
-            # draw contours on image
             cv2.drawContours(img, [blob], 0, 255, 3)
-            # print(blob[:,0,0])
     
     return img
 
@@ -35,16 +25,13 @@ def image_prep(src):
     # thing = src.copy()
     # src = cv2.fastNlMeansDenoising(thing, src, h=20)
 
-    # cv2.imshow("test", src)
-    # cv2.waitKey()
-
     # adjust "C" for adaptive threshold based on image brightness
-    # br = np.average(src)
+    br = np.average(src)
 
-    # if br < 50:
-    #     const = 1
-    # else:
-    #     const = 10
+    if br < 50:
+        const = 1
+    else:
+        const = 10
 
     # adaptive gaussian threshold
     thresh = cv2.adaptiveThreshold(src,
@@ -52,7 +39,7 @@ def image_prep(src):
                                    adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                    thresholdType=cv2.THRESH_BINARY_INV,
                                    blockSize=11,
-                                   C=10)
+                                   C=const)
 
     conts = contours(thresh)
 
@@ -62,18 +49,16 @@ def image_prep(src):
     # cv2.waitKey()
 
     # use Canny edge detector
-    # dst = cv2.Canny(src,
+    # conts = cv2.Canny(conts,
     #                 threshold1=900,
     #                 threshold2=1000,
     #                 edges=None,
     #                 apertureSize=7)
 
-    dst = conts
-
     # cv2.imshow("test", dst)
     # cv2.waitKey()
 
-    return dst
+    return conts
 
 
 def main():
@@ -85,8 +70,8 @@ def main():
 
         t1 = time.time()
 
-        _ = Image.open(f)
-        blur = _.filter(ImageFilter.BLUR)
+        # _ = Image.open(f)
+        # blur = _.filter(ImageFilter.BLUR)
 
         src = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
         dst = image_prep(src)
@@ -98,8 +83,14 @@ def main():
         # save output image
         cv2.imwrite(OUTPUT + "\\" + filename, dst)
 
-    # print(sum(ts) / len(ts))
-    print(f"Average framerate: {1 / (sum(ts) / len(ts))} fps")
+    fps = [1 / t for t in ts]
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(fps)
+    plt.show()
+
+    print("Average FPS: {}".format(np.average(fps)))
 
 
 if __name__ == "__main__":
