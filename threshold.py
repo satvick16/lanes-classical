3c17370f4c6129ae9a8b0b8c0f6c933bcb15c989
Satvick16 <satvickacharya16@gmail.com>
2022-11-26 17:51:11 -0500

    optimized code -- now at 400 fps

--- threshold.py
+++ threshold.py
@@ -9,12 +9,10 @@ OUTPUT_DIR = "C:\\Users\\16474\\Desktop\\lanes-utra\\threshold-out"
 
 
 def create_mask(img_hsv):
-    '''Create mask for red pixels using HSV color scheme'''
+    '''Create mask for white pixels using HSV color scheme'''
     sensitivity = 35
     lower_white = np.array([0,0,255-sensitivity])
     upper_white = np.array([255,sensitivity,255])
-    # lower_red = np.array([161, 155, 84])
-    # upper_red = np.array([179, 255, 255])
     mask = cv2.inRange(img_hsv, lower_white, upper_white)
 
     return mask
@@ -22,7 +20,7 @@ def create_mask(img_hsv):
 
 def get_output_img(img, mask):
     '''Convert masked image to output format'''
-    output_img = img.copy()
+    output_img = np.zeros((img.shape[0], img.shape[1]), np.uint8)
     output_img[np.where(mask == 0)] = 0
     output_img[np.where(mask != 0)] = 255
 
@@ -35,11 +33,11 @@ def main():
     # iterate through input files
     for filename in tqdm(os.listdir(INPUT_DIR)):
         f = os.path.join(INPUT_DIR, filename)
+        img = cv2.imread(f)
 
         t1 = time.time()
 
         # convert from bgr to hsv
-        img = cv2.imread(f)
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # resize image
@@ -69,13 +67,7 @@ def main():
         # save output image
         cv2.imwrite(OUTPUT_DIR + "\\" + filename, output_img)
     
-    ts = [1 / t for t in ts]
-    print("Average FPS: " + str(sum(ts) / len(ts)))
-
-    import matplotlib.pyplot as plt
-
-    plt.plot(ts)
-    plt.show()
+    print(str(len(ts) / sum(ts)) + " fps")
 
 
 if __name__ == "__main__":
